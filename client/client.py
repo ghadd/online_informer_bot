b436933e1a4b1e80f4e7f49cc86a21b752602b0b
Dan Timachov <danyatimachov@gmail.com>
2021-01-19 18:15:08 +0200

    Documenting client module.

--- client/client.py
+++ client/client.py
@@ -1,3 +1,12 @@
+"""
+This file uses telegram user api (based on telethon) in order to monitor users' online status.
+
+Restrictions
+    * As a real TG account is used as monitor, if user status is ``last seen recently``, henceforth
+    that user denied your access to his/her online status.
+"""
+from typing import Optional
+
 from telethon.sync import TelegramClient
 from telethon.tl.types import UserStatusOnline
 
@@ -8,7 +17,15 @@ logger = get_logger(__name__)
 
 
 class ClientMonitor:
-    def __init__(self, entity_qualifier):
+    """
+    This class objects are basically telegram user entities, able to check online status of given entities.
+
+    Attributes
+    ----------
+    entity_qualifier : Optional[str, int]
+        any entity identifier, determining the user entity
+    """
+    def __init__(self, entity_qualifier: Optional[str, int]):
         self.entity_qualifier = entity_qualifier
 
     @staticmethod
@@ -16,7 +33,17 @@ class ClientMonitor:
         with TelegramClient(__file__, API_ID, API_HASH) as client:
             return client.get_entity(entity_qualifier)
 
-    def is_online(self):
+    def is_online(self) -> bool:
+        """
+        Returns
+        -------
+        True is entity at self.entity_qualifier is online and False otherwise
+
+        Raises
+        ------
+        ValueError
+            When entity is not provided or it is invalid User entity
+        """
         with TelegramClient(__file__, API_ID, API_HASH) as client:
             user = None
 
