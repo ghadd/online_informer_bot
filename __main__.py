76e242d6282809a0c3686f0a7ddadefb00978c7b
Dan Timachov <danyatimachov@gmail.com>
2021-01-19 20:23:42 +0200

    Removing unused dependencies in requirements.txt

--- __main__.py
+++ __main__.py
@@ -2,7 +2,8 @@ import os
 
 # checking environment variables (all of these must be set)
 if not ("TG_API_ID" in os.environ and "TG_API_HASH" in os.environ and "ONLINE_INFORMER_BOT_TOKEN" in os.environ):
-    raise IOError('Please create files `./settings/{.api_id, .api_hash, .bot_token}` and fill them with proper values.')
+    raise IOError('Please set environmental variables TG_API_ID, TG_API_HASH and ONLINE_INFORMER_BOT_TOKEN '
+                  'with proper values.')
 
 import threading
 
