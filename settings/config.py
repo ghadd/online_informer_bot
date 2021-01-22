2cf431a766d489a2a9c793be1084b204d20deda3
Dan <danyatimachov@gmail.com>
2021-01-22 11:27:55 +0200

    Fixing getting port from env as integer.

--- settings/config.py
+++ settings/config.py
@@ -12,7 +12,7 @@ DB_NAME = os.environ["DB_NAME"]
 DB_USER = os.environ["DB_USER"]
 DB_PASS = os.environ["DB_PASS"]
 DB_HOST = os.environ["DB_HOST"]
-DB_PORT = os.environ["DB_PORT"]
+DB_PORT = int(os.environ["DB_PORT"])
 
 DEFAULT_TIMEOUT = MINUTE / 10
 DEFAULT_NOTIFY_TIMEOUT = DAY
